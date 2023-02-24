e7688b62f678e0d7b44eac894dfcada514942dac
abdenassar01 <abdenassaramimi99@gmail.com>
2023-02-24 21:57:55 +0100

    fix status bar not showing on home🤓👋

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
